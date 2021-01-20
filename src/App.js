db7123f71a10721fe1c9e295334adff8c95f6b8b
Popoy Vargas <popoyvargas@me.com>
2021-01-20 15:30:48 +0800

    Implement simple drag and drop functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className="flexbox">
+        <Board id="board1" className="board">
+          <Card id="card1" className="card" draggable><p>Card One</p></Card>
+          <Card id="card2" className="card" draggable><p>Card Two</p></Card>
+          <Card id="card3" className="card" draggable><p>Card Three</p></Card>
+        </Board>
+        <Board id="board2" className="board">
+          <Card id="card4" className="card" draggable><p>Card Four</p></Card>
+          <Card id="card5" className="card" draggable><p>Card Five</p></Card>
+        </Board>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,27 @@
+const Board = props =>
+{
+  const drop = e =>
+  {
+    e.preventDefault();
+
+    const cardId = e.dataTransfer.getData('cardId');
+    const card = document.getElementById(cardId);
+    card.style.display = 'block';
+
+    e.target.appendChild(card);
+  };
+
+  const dragOver = e =>
+  {
+    e.preventDefault();
+  };
+
+  return (
+    <div id={ props.id } className={ props.className } onDrop={ drop }
+      onDragOver={ dragOver }>
+      { props.children }
+    </div>
+  );
+};
+
+export default Board;
